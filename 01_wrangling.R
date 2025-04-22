cd0afb7ecaa159dc0f9d662137da9988f684536c
hultingk <hultingk@msu.edu>
2025-04-22 15:05:13 -0400

    adding dispersal mode info

--- 01_wrangling.R
+++ 01_wrangling.R
@@ -11,6 +11,7 @@ patch_info <- read.csv(file = file.path("data", "L0_original", "corridor_patch_i
 year_created <- read.csv(file = file.path("data", "L0_original", "year_site_creation.csv"))
 dispersal_mode <- read.csv(file = file.path("data", "L0_original", "dispersal_mode_20250418.csv"))
 
+
 # making column names lower case for joining and preference
 colnames(srs_all) <- stringr::str_to_lower(colnames(srs_all))
 
@@ -24,6 +25,7 @@ patch_info <- patch_info %>%
 dispersal_mode <- dispersal_mode %>%
   dplyr::select(c("Species.Code", "USDACode", "DispMode1", "DispMode2"))
 
+
 # adding patch info, year, and dispersal mode to plant data
 srs_all <- srs_all %>%
   left_join(year_created, by = c("block")) %>%
@@ -31,23 +33,54 @@ srs_all <- srs_all %>%
   left_join(dispersal_mode, by = c("sppcode" = "Species.Code"))
 
 
+
+
+# matching dispersal mode to EDI dispersal mode to look at missing values #
+# loading species info from EDI
+#edi_dispersal_url <- "https://pasta.lternet.edu/package/data/eml/edi/414/1/8de4a490a6ac6b05d2406c975d25b649"
+#edi_dispersal <- read.csv(file = srs_dispersal_url)
+
+#srs_all %>%
+#  filter(is.na(DispMode1)) %>% # looking to see which species don't have dispersal mode info
+#  count(sppcode) %>%
+#  left_join(edi_dispersal, by = c("sppcode" = "SppCode")) # looking to see if EDI data solves any of these
+
+# adding missing dispersal modes
+srs_all <- srs_all %>%
+  mutate(DispMode1 = dplyr::case_when(
+    sppcode %in% c("ALLCUT", "GALMOL", "MANVIR", "PASSPP", "STRUMB") ~ "Gravity",
+    sppcode %in% c("ANDSPP", "CATBIG", "CIRHOR", "CUSSPP", 
+                   "LIASCG", "OSMREG", "SABDIF", "SENSMA", "TRAGOP") ~ "Wind",
+    sppcode %in% c("CARSPP", "GALSPP", "LANTAN", "PLUSPP", "SOLNIG") ~ "Animal",
+    .default = DispMode1
+  ))
+
+
+# grouping dispersal modes into gravity, wind, or animal dispersed
+srs_all <- srs_all %>%
+  mutate(dispersal_mode = dplyr::case_when(
+    DispMode1 %in% c("Adhesive", "Animal", "Ant", "Bird", "Mammal", "MammalBird") ~ "Animal",
+    DispMode1 %in% c("Tumbling", "Wind") ~ "Wind",
+    DispMode1 %in% c("Ballistic", "Gravity", "Unassisted") ~ "Gravity",
+    .default = DispMode1
+  ))
+
+
 # adding survey time since patch creation
 srs_all <- srs_all %>%
   mutate(time = year - year.created)
 
-# checking for missing values and duplicates
-summarytools::view(summarytools::dfSummary(srs_all), footnote = NA)
 
 
 # renaming, removing unneeded columns, and rearranging
 srs_all <- srs_all %>%
   mutate(year_created = year.created) %>% # renaming
-  mutate(dipsersal_mode = DispMode1) %>% # renaming
-  mutate(dipsersal_mode2 = DispMode2) %>% # renaming
   mutate(unique_id = paste(block, patch, patch_type, sep = "-")) %>% # creating unique ID for each patch
   dplyr::select(block, patch, cell, patch_type, unique_id, core, year, year_created, time,
-                sppcode, dipsersal_mode, dipsersal_mode2)
+                sppcode, dispersal_mode)
 
+# checking for missing values and duplicates
+summarytools::view(summarytools::dfSummary(srs_all), footnote = NA)
 
 # writing cleaned file
 write_csv(srs_all, file = file.path("data", "L1_wrangled", "srs_plant_all.csv"))
--- 02_local-stability.R
+++ 02_local-stability.R
@@ -1,19 +1,12 @@
 # loading libraries
-# Load `librarian` package
-library(librarian)
-# Install missing packages and load needed libraries
-shelf(tidyverse, vegan)
+library(librarian) # Load `librarian` package
+shelf(tidyverse, vegan) # Install missing packages and load needed libraries
 
 # loading data
 srs_data <- read_csv(file = file.path("data", "L1_wrangled", "srs_plant_all.csv"))
 
-# splitting dataframe into each patch as a list for function
-srs_data_split <- srs_data %>% 
-  dplyr::count(unique_id, time, sppcode) %>%
-  group_by(unique_id) %>%
-  group_split()
 
-# writing function to calculate jaccard's dissimilarity iteratively between consectutive years
+# writing function to calculate jaccard's dissimilarity iteratively between consecutive years
 compute_jaccard <- function(df) {
   # convert data to correct format
   df_wide <- df %>% 
@@ -40,47 +33,61 @@ compute_jaccard <- function(df) {
   return(result)
 }
 
+
+#### ALL SPECIES ####
+# splitting dataframe into each patch as a list for function
+srs_data_split <- srs_data %>% 
+  dplyr::count(unique_id, time, sppcode) %>%
+  group_by(unique_id) %>%
+  group_split()
+
 # applying function to data
 jaccard_results <- srs_data_split %>% 
   lapply(compute_jaccard) %>%
   bind_rows() # putting together into a dataframe
 
-# converting year pairs into a usable format for modeling/plotting
-jaccard_results <- jaccard_results %>%
-  separate(year_pair, into = c("time1", "time2"), sep = " - ", remove = FALSE) %>%
-  mutate(time1 = as.numeric(time1)) %>%
-  mutate(time2 = as.numeric(time2)) %>%
-  mutate(years_bw_surveys = time2 - time1)
-  
-jaccard_results <- jaccard_results %>%
-  separate(unique_id, into = c("block", "patch", "patch_type"), sep = "-", remove = F) # separating unique ID into columns
 
+#### COMMON SPECIES ####
+# repeating excluding rare species (species with less than 10 occurrences across all surveys)
 
-
-
-
-# repeating excluding rare species (species with less than 10 occurances across all surveys)
-common_spp <- srs_data %>%
-  count(sppcode) %>%
-  arrange(n) %>%
-  filter(n > 10) %>%
-  mutate(rare = 0) %>%
-  select(!c("n"))
+# figuring out which species are common, assigning a value to those to keep later 
+common_spp <- srs_data %>% 
+  count(sppcode) %>% # counting # of occurrences per species across all surveys
+  filter(n > 10) %>% # removing species that had less than 10 occurrences
+  mutate(rare = 0) %>% #  assigning a 0 to common species, rare will be NA later when joining
+  dplyr::select(!c("n")) # removing counts of occurrences
 
 common_spp_jaccard <- srs_data %>%
-  left_join(common_spp, by = c("sppcode")) %>%
-  filter(rare == 0) %>%
-  dplyr::select(!c("rare")) %>%
-  dplyr::count(unique_id, time, sppcode) %>%
+  left_join(common_spp, by = c("sppcode")) %>% # joining data on which species are common
+  filter(rare == 0) %>% # only keeping common species
+  dplyr::select(!c("rare")) %>% # removing column
+  dplyr::count(unique_id, time, sppcode) %>% # preparing data for function
   group_by(unique_id) %>%
-  group_split() %>%
-  lapply(compute_jaccard) %>%
-  bind_rows() # putting together into a dataframe
+  group_split() %>% # splitting dataframe into each patch as a list for function
+  lapply(compute_jaccard) %>% # applying function to common species
+  bind_rows() %>% # putting together into a dataframe
+  rename(COMMON_jaccard = jaccard_dissimilarity, # renaming columns to be different than calculations with all species
+         COMMON_turnover = turnover_values, 
+         COMMON_nestedness = nestedness_values)
 
-common_spp_jaccard <- common_spp_jaccard %>%
-  separate(unique_id, into = c("block", "patch", "patch_type"), sep = "-") %>%
-  separate(year_pair, into = c("time1", "time2"), sep = " - ", remove = FALSE) %>%
-  mutate(time1 = as.numeric(time1)) %>%
-  mutate(time2 = as.numeric(time2)) %>%
-  mutate(years_bw_surveys = time2 - time1)
+#### LONGLEAF SPECIES (excluding rare) ####
+
+
+#### DISPERSAL MODE (excluding rare) ####
+
+
+
+
+
+
+#### joining all together ####
+jaccard_results <- jaccard_results %>%
+  left_join(common_spp_jaccard, by = c("unique_id", "year_pair")) %>% # joining with other data
+  separate(unique_id, into = c("block", "patch", "patch_type"), sep = "-", remove = F) %>% # separating unique ID into columns
+  separate(year_pair, into = c("time1", "time2"), sep = " - ", remove = FALSE) %>% # converting year pairs into a usable format for modeling/plotting
+  mutate(time1 = as.numeric(time1)) %>% # converting to numeric
+  mutate(time2 = as.numeric(time2)) %>% # converting to numeric
+  mutate(years_bw_surveys = time2 - time1) # calculating # of years between surveys (most are annual)
+
+
 
