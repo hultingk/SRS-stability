70fd288b319c0cb66f7c6a3f3b251ee6ef98bcb3
hultingk <hultingk@msu.edu>
2025-04-24 16:09:55 -0400

    adding soil moisture calculations, more dispersal mode info

--- 01_wrangling.R
+++ 01_wrangling.R
@@ -23,6 +23,8 @@ patch_info <- patch_info %>% # for patch info
   mutate(block = if_else(block == "8", "08", block))
 soil_moisture_2007 <- soil_moisture_2007 %>% # and for soil moisture data
   mutate(block = if_else(block == "8", "08", block))
+soil_moisture_2003 <- soil_moisture_2003 %>% # and for soil moisture data
+  mutate(EU = if_else(EU == "8", "08", EU))
 
 # getting rid of columns of dispersal mode that I don't need
 dispersal_mode <- dispersal_mode %>%
@@ -37,7 +39,6 @@ srs_all <- srs_all %>%
 
 
 
-
 # matching dispersal mode to EDI dispersal mode to look at missing values #
 # loading species info from EDI
 #edi_dispersal_url <- "https://pasta.lternet.edu/package/data/eml/edi/414/1/8de4a490a6ac6b05d2406c975d25b649"
@@ -48,14 +49,26 @@ srs_all <- srs_all %>%
 #  count(sppcode) %>%
 #  left_join(edi_dispersal, by = c("sppcode" = "SppCode")) # looking to see if EDI data solves any of these
 
+#### spp codes, dispersal modes ####
+# resolving spp codes
+srs_all <- srs_all %>%
+  mutate(sppcode == dplyr::case_when(
+    sppcode %in% c("DESPER") ~ "DESGLA", # changing DESPER to DESGLA at Christopher's suggestion -- email 05/24/2025
+    sppcode %in% c("SOLPTY", "SOLNIG") ~ "SOLNIG", # grouping these at Christopher's suggestion -- email 05/24/2025
+    sppcode %in% c("DIGSSPP") ~ "DIGSPP", # fixing type -- 7 letter code DIGSSPP to 6 letter code DIGSPP
+    .default = sppcode
+  ))
 
 # adding missing dispersal modes -- doing this manually, going to delete EDI portion later
 srs_all <- srs_all %>%
   mutate(DispMode1 = dplyr::case_when(
-    sppcode %in% c("ALLCUT", "GALMOL", "MANVIR", "PASSPP", "STRUMB") ~ "Gravity",
+    sppcode %in% c("ALLCUT", "GALMOL", "MANVIR", "PASSPP", "STRUMB", 
+                   "BAPALS", "CHEALB", "DICSPP", "DIGSPP", "RHYSPP") ~ "Gravity",
     sppcode %in% c("ANDSPP", "CATBIG", "CIRHOR", "CUSSPP", 
-                   "LIASCG", "OSMREG", "SABDIF", "SENSMA", "TRAGOP") ~ "Wind",
-    sppcode %in% c("CARSPP", "GALSPP", "LANTAN", "PLUSPP", "SOLNIG") ~ "Animal",
+                   "LIASCG", "OSMREG", "SABDIF", "SENSMA", "TRAGOP",
+                   "HYPSPP") ~ "Wind",
+    sppcode %in% c("CARSPP", "GALSPP", "LANTAN", "PLUSPP", "SOLNIG", 
+                   "DESLRG", "DESGLA", "ILEDEC") ~ "Animal",
     .default = DispMode1
   ))
 
@@ -69,38 +82,71 @@ srs_all <- srs_all %>%
     .default = DispMode1
   ))
 
+# fixing transplant column
+srs_all <- srs_all %>%
+  mutate(transplant = dplyr::case_when(
+    sppcode %in% c("ARIBEY", "SORSEC", "ANTVIL", "CARBEL", "LIAEAR", "PHYAME",
+                   "SOLAME", "NOLGEO", "GAYDUM", "RUDHIR", "LANCAM", "ILEVER") ~ TRUE,
+    .default = FALSE
+  )) %>%
+  dplyr::select(!c("transplant.")) # removing old transplant column
+
+
 
 # adding survey time since patch creation
 srs_all <- srs_all %>%
   mutate(time = year - year.created)
 
+#### soil moisture ####
 # soil moisture data -- need to decide how to combine -- use 2007 for all blocks except the 75s??
-### NOTE FOR LATER: check to see how different values are between 2003 and 2007
-soil_moisture_2003 %>%
-  count(EU, Patch)
-summarytools::view(summarytools::dfSummary(soil_moisture_2003), footnote = NA)
+# data from 2003
+soil_moisture_2003 <- soil_moisture_2003 %>%
+  rename(block = EU, patch = Patch, plot = Plot) %>%
+  group_by(block, patch) %>%
+  summarise(soil_moisture_2003 = mean(Pct_moisture_by_wt))
 
-soil_moisture_2007 %>%
-  count(block, patch)
-summarytools::view(summarytools::dfSummary(soil_moisture_2007), footnote = NA)
-# NEED TO ADD SOIL MOISTURE DATA LATER
+# data from 2007
+soil_moisture_2007 <- soil_moisture_2007 %>%
+  mutate(wet = wet.weight - tin.plus.filter.paper) %>%
+  mutate(dry = dry.weight - tin.plus.filter.paper) %>%
+  mutate(soil_moisture = (wet-dry)/dry*100) %>%
+  filter(wet.weight < 22500.00) %>% # removing outlier
+  filter(wet.weight > 116) %>% # removing outlier, dry weight more than wet weight
+  group_by(block, patch) %>%
+  summarise(soil_moisture_2007 = mean(soil_moisture))
+
+# joining 2003 and 2007 data together
+soil_moisture <- soil_moisture_2007 %>%
+  full_join(soil_moisture_2003, by = c("block", "patch"))
+
+# looking ar correlations between soil moisture values between 2003 and 2007
+soil_moisture2 <- soil_moisture %>%
+  filter(!is.na(soil_moisture_2003)) %>%
+  filter(!is.na(soil_moisture_2007))
+cor(soil_moisture2$soil_moisture_2007, soil_moisture2$soil_moisture_2003) # not that great, ~0.6 correlation
+
+# deciding to use 2007 data, except for the 75s which will use 2003 data
+soil_moisture <- soil_moisture %>%
+  mutate(soil_moisture = if_else(is.na(soil_moisture_2007), soil_moisture_2003, soil_moisture_2007))
+
+# joining to full dataset
+srs_all <- srs_all %>%
+  left_join(soil_moisture, by = c("block", "patch"))
 
 
+#### final dataset ####
 # renaming, removing unneeded columns, and rearranging
 srs_all <- srs_all %>%
   mutate(year_created = year.created) %>% # renaming
   mutate(unique_id = paste(block, patch, patch_type, sep = "-")) %>% # creating unique ID for each patch
-  dplyr::select(block, patch, cell, patch_type, unique_id, core, year, year_created, time,
-                sppcode, dispersal_mode)
+  dplyr::select(block, patch, cell, patch_type, unique_id, soil_moisture, core, year, year_created, time,
+                sppcode, transplant, dispersal_mode)
 
 # checking for missing values and duplicates
 summarytools::view(summarytools::dfSummary(srs_all), footnote = NA)
 
 # writing cleaned file
 write_csv(srs_all, file = file.path("data", "L1_wrangled", "srs_plant_all.csv"))
-
-
-
 
 
 
