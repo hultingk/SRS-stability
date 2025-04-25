6d6f1eee423f377a061adf2b7971e48cda1b625f
hultingk <hultingk@msu.edu>
2025-04-25 14:52:20 -0400

    adding script

---
+++ 02b_metacommunity stability.R
@@ -0,0 +1,9 @@
+# loading libraries
+library(librarian) # Load `librarian` package
+shelf(tidyverse, vegan) # Install missing packages and load needed libraries
+
+# loading data
+srs_data <- read_csv(file = file.path("data", "L1_wrangled", "srs_plant_all.csv"))
+
+srs_data <- srs_data %>% # removing experimentally planted species 
+  filter(transplant != TRUE)
