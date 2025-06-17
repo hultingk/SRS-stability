49ddbda9c56a07500f97b5660c81185178eafb49
hultingk <hultingk@msu.edu>
2025-06-17 11:49:42 -0400

    splitting up segment and directionality analysis

--- 04a_cta_segments.R
+++ 04a_cta_segments.R
@@ -1,4 +1,4 @@
-### community trajectory analysis
+### community trajectory analysis - segement lengths
 librarian::shelf(tidyverse, vegan, ecotraj, glmmTMB, DHARMa, emmeans, ggeffects, AICcmodavg) # Install missing packages and load needed libraries
 
 # loading data
@@ -119,117 +119,8 @@ segment_lengths_plot
 # dev.off()
 
 
-# trajectory directionality
-# segment_direction <- trajectoryDirectionality(srs_trajectory)
-# segment_direction <- data.frame(segment_direction)
-# segment_direction <- segment_direction %>%
-#   rownames_to_column("unique_id") %>%
-#   separate(unique_id, into = c("block", "patch", "patch_type")) 
 
 
 
-
-
-
-
-
-###### directionality in first decade since patch creation #####
-sp_info_1_12 <- srs_data_wider %>%
-  filter(time <= 12)
-
-patch_info_1_12 <- sp_info_1_12 %>%
-  arrange(unique_id, time) %>%
-  select(unique_id, time, year)
-
-# species matrix
-sp_info_1_12 <- sp_info_1_12 %>%
-  arrange(unique_id, time) %>%
-  mutate(unique_id_year = paste(unique_id, time, year, sep = "-")) %>%
-  column_to_rownames("unique_id_year") %>%
-  select(!c("unique_id", "time", "year"))
-
-# Jaccard distance matrix
-jaccard_dist_1_12 <- vegdist(sp_info_1_12, method = "jaccard")
-
-# defining trajectories
-srs_trajectory_1_12 <- defineTrajectories(jaccard_dist_1_12, sites = patch_info_1_12$unique_id, surveys = patch_info_1_12$time)
-
-# directionality
-segment_direction_1_12 <- trajectoryDirectionality(srs_trajectory_1_12)
-segment_direction_1_12 <- data.frame(segment_direction_1_12)
-segment_direction_1_12 <- segment_direction_1_12 %>%
-  rownames_to_column("unique_id") %>%
-  separate(unique_id, into = c("block", "patch", "patch_type")) %>%
-  mutate(time = "Year 1-12") %>%
-  rename(directionality = segment_direction_1_12)
-
-
-
-
-
-###### directionality in SECOND decade since patch creation #####
-sp_info_13_24 <- srs_data_wider %>%
-  filter(time >= 13)
-
-patch_info_13_24 <- sp_info_13_24 %>% 
-  arrange(unique_id, time) %>%
-  select(unique_id, time, year)
-
-# species matrix
-sp_info_13_24 <- sp_info_13_24 %>%
-  arrange(unique_id, time) %>%
-  mutate(unique_id_year = paste(unique_id, time, year, sep = "-")) %>%
-  column_to_rownames("unique_id_year") %>%
-  select(!c("unique_id", "time", "year"))
-
-# Jaccard distance matrix
-jaccard_dist_13_24 <- vegdist(sp_info_13_24, method = "jaccard")
-
-# defining trajectories
-srs_trajectory_13_24 <- defineTrajectories(jaccard_dist_13_24, sites = patch_info_13_24$unique_id, surveys = patch_info_13_24$time)
-
-# directionality
-segment_direction_13_24 <- trajectoryDirectionality(srs_trajectory_13_24)
-segment_direction_13_24 <- data.frame(segment_direction_13_24)
-segment_direction_13_24 <- segment_direction_13_24 %>%
-  rownames_to_column("unique_id") %>%
-  separate(unique_id, into = c("block", "patch", "patch_type")) %>%
-  mutate(time = "Year 13-24") %>%
-  rename(directionality = segment_direction_13_24)
-
-
-#### putting all together
-segment_direction_all <- rbind(
-  segment_direction_1_12,
-  segment_direction_13_24
-)
-
-segment_direction_plot <- segment_direction_all %>%
-  mutate(patch_type = dplyr::case_when(
-    patch_type %in% c("connected") ~ "Connected",
-    patch_type %in% c("rectangle") ~ "Rectangular",
-    patch_type %in% c("wing") ~ "Winged"
-  )) %>%
-  ggplot(aes(time, directionality, fill = patch_type)) +
-  #geom_point(aes(color = patch_type), size=2, alpha=0.9, position = position_jitterdodge(dodge.width = 1)) +
-  geom_boxplot(aes(fill = patch_type)) +
-  theme_minimal(base_size = 28) +
-  scale_fill_manual(values = c("#5389A4", "#CC6677", "#DCB254"), name = "Patch Type") +
-  scale_color_manual(values = c("#5389A4", "#CC6677", "#DCB254"), name = "Patch Type") +
-  #scale_fill_brewer(palette = "Set2", name = "Patch Type") +
-  ylab("Trajectory directionality") +
-  xlab("Time period")
-segment_direction_plot
-
-pdf(file = file.path("plots", "segment_direction.pdf"), width = 12, height = 8)
-segment_direction_plot
-dev.off()
-
-
-# model
-m.direction <- glmmTMB(directionality ~ patch_type * time + (1|block/patch),
-                       data = segment_direction_all)
-summary(m.direction)
-plot(simulateResiduals(m.direction))
 
 
