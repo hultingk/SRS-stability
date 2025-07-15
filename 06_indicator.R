840142a6a5d0843cb766491f482b85f94ce4f007
hultingk <hultingk@msu.edu>
2025-07-15 09:18:33 -0400

    indicator sp analysis

--- 06_indicator.R
+++ 06_indicator.R
@@ -7,6 +7,8 @@ patch_info <- patch_info %>%
 
 
 indicator <- multipatt(sp_info, cluster = patch_info$patch_type,
+                       func = "IndVal.g",
                        control = how(nperm = 999))
 
 summary(indicator)
+coverage(sp_info, indicator, At = 0.8, alpha = 0.05)
