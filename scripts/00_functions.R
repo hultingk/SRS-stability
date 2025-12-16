50cdcadf8b9ba0f59f5e05de91aee74b9607ea10
hultingk <hultingk@msu.edu>
2025-12-16 13:00:24 -0500

    annotating

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -1,3 +1,10 @@
+########
+## SCRIPT NAME: 00_functions.R
+## AUTHOR: Katherine Hulting
+## PURPOSE: Script containing custom functions used throughout analysis.
+## PRODUCTS: NA
+#########
+
 # loading libraries
 librarian::shelf(tidyverse, vegan) # Install missing packages and load needed libraries
 
@@ -7,26 +14,26 @@ compute_convergence_jaccard <- function(df) {
   df_wide <- df %>% 
     pivot_wider(names_from = sppcode, values_from = n, values_fill = 0) %>% # wide format
     arrange(unique_id, time) %>%  # Ensure years are sorted properly
-    mutate(unique_id = paste(unique_id, time, sep = "-")) %>%
-    dplyr::select(-block, -patch_type, -patch, -year, -time) %>%
-    column_to_rownames("unique_id")
+    mutate(unique_id = paste(unique_id, time, sep = "-")) %>% # create unique ID
+    dplyr::select(-block, -patch_type, -patch, -year, -time) %>% # drop unnecessary columns
+    column_to_rownames("unique_id") # make unique ID the rownames
   
-  jac <- vegdist(df_wide, method = "jaccard")
-  jac_df <- as.data.frame(as.table(as.matrix(jac)))
-  names(jac_df) <- c("unique_id1", "unique_id2", "jaccard")
+  jac <- vegdist(df_wide, method = "jaccard") # jaccard dissimilarity
+  jac_df <- as.data.frame(as.table(as.matrix(jac))) # make dataframe
+  names(jac_df) <- c("unique_id1", "unique_id2", "jaccard") # set names
   
   jac_df <- jac_df %>%
-    separate(unique_id1, into = c("block1", "patch_rep1", "patch_type1", "time1"), remove = T) %>%
-    separate(unique_id2, into = c("block2", "patch_rep2", "patch_type2", "time2"), remove = T) %>%
-    mutate(unique_id1 = paste(block1, patch_rep1, patch_type1, sep = "-")) %>%
-    mutate(unique_id2 = paste(block2, patch_rep2, patch_type2, sep = "-")) %>%
-    filter(!(unique_id1 == unique_id2)) %>%
-    filter(time1 == time2) %>%
-    mutate(patch_pair = paste(patch_type1, patch_type2, sep = "-")) %>%
-    dplyr::select(-block2, -time2, -patch_rep1, -patch_rep2, -patch_type1, -patch_type2) %>%
-    rename(block = block1, time = time1) %>%
-    mutate(time = as.numeric(time)) %>%
-    mutate(patch_pair = dplyr::case_when(
+    separate(unique_id1, into = c("block1", "patch_rep1", "patch_type1", "time1"), remove = T) %>% # seperate unique ID of patch 1 into its parts
+    separate(unique_id2, into = c("block2", "patch_rep2", "patch_type2", "time2"), remove = T) %>% # seperate unique ID of patch 2 into its parts
+    mutate(unique_id1 = paste(block1, patch_rep1, patch_type1, sep = "-")) %>% # recreate unique ID without time
+    mutate(unique_id2 = paste(block2, patch_rep2, patch_type2, sep = "-")) %>% # recreate unique ID without time
+    filter(!(unique_id1 == unique_id2)) %>% # remove observations comparing patch to itself
+    filter(time1 == time2) %>% # only keeping observations that are comparing patches in the same year
+    mutate(patch_pair = paste(patch_type1, patch_type2, sep = "-")) %>% # creating variable definining which patch types are being compared
+    dplyr::select(-block2, -time2, -patch_rep1, -patch_rep2, -patch_type1, -patch_type2) %>% # dropping irrelevant variables
+    rename(block = block1, time = time1) %>% # renaming 
+    mutate(time = as.numeric(time)) %>% # making numeric
+    mutate(patch_pair = dplyr::case_when( # making sure patch type comparison character strings are consistent
       patch_pair %in% c("Connected-Rectangular", "Rectangular-Connected") ~ "Connected-Rectangular",
       patch_pair %in% c("Connected-Winged", "Winged-Connected") ~ "Connected-Winged",
       patch_pair %in% c("Rectangular-Winged", "Winged-Rectangular") ~ "Rectangular-Winged",
@@ -36,7 +43,7 @@ compute_convergence_jaccard <- function(df) {
       .default = patch_pair
     ))
   
-  return(jac_df)
+  return(jac_df) # returning dataframe
 }
 
 
@@ -52,22 +59,22 @@ pairwise_ecopart <- function(nested_list, components) {
     comp_res <- map(comp_names, function(cmp) {
       mat1 <- nested_list[[as.character(t1)]][[cmp]]
       mat2 <- nested_list[[as.character(t2)]][[cmp]]
-      # drop metadata columns, keep only species
-      mat1 <- mat1 %>% dplyr::select(-block, -unique_id, -time, -unique_id_time)
-      mat2 <- mat2 %>% dplyr::select(-block, -unique_id, -time, -unique_id_time)
-      # make sure same species order
-      common_species <- intersect(names(mat1), names(mat2))
-      mat1 <- mat1[, common_species, drop = FALSE]
-      mat2 <- mat2[, common_species, drop = FALSE]
       
-      ecopart::ecopart.pair(mat1, mat2, components = components, index = "jaccard")
+      mat1 <- mat1 %>% dplyr::select(-block, -unique_id, -time, -unique_id_time) # drop metadata columns, keep only species
+      mat2 <- mat2 %>% dplyr::select(-block, -unique_id, -time, -unique_id_time) # drop metadata columns, keep only species
+      
+      common_species <- intersect(names(mat1), names(mat2))
+      mat1 <- mat1[, common_species, drop = FALSE] # make sure same species order
+      mat2 <- mat2[, common_species, drop = FALSE]# make sure same species order
+      
+      ecopart::ecopart.pair(mat1, mat2, components = components, index = "jaccard") # applying ecopart function
     })
     
     names(comp_res) <- comp_names
     comp_res
   })
   
-  names(results) <- paste(time_keys[-length(time_keys)], time_keys[-1], sep = "_vs_")
+  names(results) <- paste(time_keys[-length(time_keys)], time_keys[-1], sep = "_vs_") # returning results with correct time comparisons
   results
 }
 
@@ -93,8 +100,8 @@ results_to_df <- function(results) { # taking a nested list and flattening it - 
 
 #### calculate raw composition change between consecutive years ####
 compute_composition_change <- function(df) {
-  # convert data to correct format
-  df_wide <- df %>% 
+  
+  df_wide <- df %>% # convert data to correct format
     pivot_wider(names_from = sppcode, values_from = n, values_fill = 0) %>% # wide format
     arrange(unique_id, time) %>%  # ensure years are sorted properly
     mutate(unique_id = paste(unique_id, time, sep = "-")) %>%
@@ -105,9 +112,9 @@ compute_composition_change <- function(df) {
   turnover <- df_wide %>%
     arrange(time) %>% # ensure years are sorted properly
     mutate(
-      gains = rowSums(across(-time, ~ (.x == 1 & lag(.x) == 0)), na.rm = TRUE),     # calculate sp gains, where 0 -> 1
-      losses = rowSums(across(-time, ~ (.x == 0 & lag(.x) == 1)), na.rm = TRUE),    # calculate sp losses, 1 -> 0
-      stayed_present = rowSums(across(-time, ~ (.x == 1 & lag(.x) == 1)), na.rm = TRUE),  # calculate sp staying consistent, 1 -> 1
+      gains = rowSums(across(-time, ~ (.x == 1 & lag(.x) == 0)), na.rm = TRUE), # calculate sp gains, where 0 -> 1
+      losses = rowSums(across(-time, ~ (.x == 0 & lag(.x) == 1)), na.rm = TRUE), # calculate sp losses, 1 -> 0
+      stayed_present = rowSums(across(-time, ~ (.x == 1 & lag(.x) == 1)), na.rm = TRUE), # calculate sp staying consistent, 1 -> 1
       changed_total = gains + losses # total turnover
     ) %>%
     dplyr::select(time, gains, losses, stayed_present, changed_total)
