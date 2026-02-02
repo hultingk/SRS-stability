489bd66b6da1f79f218608255c5eabf93f0c588e
hultingk <hultingk@msu.edu>
2026-02-02 15:18:14 -0500

    updating gitignore

--- discarded_scripts/trash-comparing-ediR.r
+++ discarded_scripts/trash-comparing-ediR.r
@@ -1,4 +1,4 @@
-
+librarian::shelf(tidyverse, summarytools)
 
 srs_data <- read_csv(file = file.path("data", "L1_wrangled", "srs_plant_all.csv"))
 
@@ -11,13 +11,14 @@ edi_srs_data2 <- edi_srs_data %>%
   select(!X1:X8) %>% # removing plot info - redundant
   filter(occurance == 1)
 
-# loading species info from EDI
-srs_dispersal_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=edi.414.1&entityid=8de4a490a6ac6b05d2406c975d25b649"
-edi_srs_dispersal <- read.csv(file = srs_dispersal_url)
 
 
-# comparing species lists
+# comparing species lists - taking out transplants and center patch to be consistent with 2019 data
 new_spp_list <- srs_data %>%
+  filter(patch_type != "Center") %>%
+  filter(transplant != TRUE) %>%
+  filter(year <= 2017) %>%
+  filter(!(block == "54N" & year %in% c("2015", "2017"))) %>%
   count(sppcode, transplant) %>%
   mutate(data1 = "2024")
 
@@ -33,17 +34,21 @@ edi_spp_list %>%
   anti_join(edi_spp_list2, by = "SppCode")
 
 all_spp_list <- new_spp_list %>%
-  full_join(edi_spp_list, by = c("sppcode" = "SppCode")) %>%
-  dplyr::select(sppcode, data1, data2, transplant) %>%
+  full_join(edi_spp_list2, by = c("sppcode" = "SppCode")) %>%
+  dplyr::select(sppcode, data1, data2) %>%
   filter(is.na(data1) | is.na(data2))
 
-
+write.csv(all_spp_list, file = file.path("data", "L0_original", "spp_compare.csv"))
 
 # blocks and years
 edi_blocks <- edi_srs_data %>%
   count(EU, Year)
 edi_blocks$EU <- substring(edi_blocks$EU, 3)
-new_blocks <- srs_data %>%
+
+new_blocks <-srs_data %>%
+  filter(patch_type != "Center") %>%
+  filter(transplant != TRUE) %>%
+  filter(year <= 2017) %>%
   count(block, year)
 
 new_blocks %>%
